3934f896d334fd8dff193fb74d92a19964af5926
kmj990929 <kmj990929@dgist.ac.kr>
2020-12-29 18:41:17 +0900

    feat: countUp 기능 구현

--- app/src/main/java/com/example/helloworld/MainActivity.kt
+++ app/src/main/java/com/example/helloworld/MainActivity.kt
@@ -14,6 +14,10 @@ class MainActivity : AppCompatActivity() {
         rollButton.setOnClickListener{
             rollDice()
         }
+        val countUpButton : Button = findViewById(R.id.count_up_button)
+        countUpButton.setOnClickListener{
+            countUp()
+        }
     }
 
     private fun rollDice() {
@@ -21,5 +25,20 @@ class MainActivity : AppCompatActivity() {
         val resultText: TextView = findViewById(R.id.result_text)
         val randomInt = (1..6).random()
         resultText.text = randomInt.toString()
+    }
+
+    private fun countUp() {
+        Toast.makeText(this, "countUp button clicked", Toast.LENGTH_SHORT).show()
+        val countText: TextView = findViewById(R.id.result_text)
+        if (countText.text == "Hello World!") {
+            countText.text = 1.toString()
+        }
+        else {
+            var count: Int = countText.text.toString().toInt()
+            if (count < 6) {
+                count += 1
+                countText.text = count.toString()
+            }
+        }
     }
 }
