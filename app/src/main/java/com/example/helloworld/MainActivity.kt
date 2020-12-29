ab1296be6c5c2ba8725bfca935ead02abd377510
kmj990929 <kmj990929@dgist.ac.kr>
2020-12-29 19:04:12 +0900

    feat: 주사위 이미지로 표시하는 기능 구현

--- app/src/main/java/com/example/helloworld/MainActivity.kt
+++ app/src/main/java/com/example/helloworld/MainActivity.kt
@@ -3,42 +3,33 @@ package com.example.helloworld
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.ImageView
 import android.widget.TextView
 import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var diceImage : ImageView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         val rollButton : Button = findViewById(R.id.roll_button)
-        rollButton.setOnClickListener{
-            rollDice()
-        }
-        val countUpButton : Button = findViewById(R.id.count_up_button)
-        countUpButton.setOnClickListener{
-            countUp()
-        }
+        rollButton.setOnClickListener{ rollDice() }
+        diceImage = findViewById(R.id.dice_image)
     }
 
     private fun rollDice() {
         Toast.makeText(this, "button clicked", Toast.LENGTH_SHORT).show()
-        val resultText: TextView = findViewById(R.id.result_text)
         val randomInt = (1..6).random()
-        resultText.text = randomInt.toString()
-    }
-
-    private fun countUp() {
-        Toast.makeText(this, "countUp button clicked", Toast.LENGTH_SHORT).show()
-        val countText: TextView = findViewById(R.id.result_text)
-        if (countText.text == "Hello World!") {
-            countText.text = 1.toString()
+        val drawableResource = when (randomInt) {
+            1 -> R.drawable.dice_1
+            2 -> R.drawable.dice_2
+            3 -> R.drawable.dice_3
+            4 -> R.drawable.dice_4
+            5 -> R.drawable.dice_5
+            else -> R.drawable.dice_6
         }
-        else {
-            var count: Int = countText.text.toString().toInt()
-            if (count < 6) {
-                count += 1
-                countText.text = count.toString()
-            }
-        }
+        diceImage.setImageResource(drawableResource)
     }
 }
